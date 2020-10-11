73923ab499c7616ec6bc0809934611e2abb73518
Diaconescu Maria Raluca <44669618+Sofia190@users.noreply.github.com>
2020-10-11 20:11:14 +0300

    Added Searches app

---
+++ Searches/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+
+# Register your models here.
+
+
+from .models import SearchQuery
+
+
+admin.site.register(SearchQuery)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Searches/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from .models import SearchQuery
+
+from Translator.models import Translator
+
+# Create your views here.
+
+
+
+def search_view(request):
+	query = request.GET.get('q', None)
+	user = None 
+	if request.user.is_authenticated:
+		user = request.user
+		context = {'query' : query}
+		if query is not None:
+			SearchQuery.objects.create(user=user, query=query)
+			translators = Translator.objects.search(query=query)    
+			context['translators'] = translators
+		return render(request, 'Searches/view.html', context)
+
+	else:
+		return render(request, "index_simple_layout_login.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
